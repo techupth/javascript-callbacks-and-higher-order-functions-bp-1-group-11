af04f4538e0a86a24686d26134760fa3d0b82a03
Babygungle <apredikanit@gmail.com>
2023-10-08 22:23:59 +0700

    Blue's work

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,16 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
-}
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(operation(array[i]));
+  }
+  return newArray;
+  }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
+newEmployeeSalaries.push(forEach(employeeSalaries, (salary) => salary + 5000));
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
